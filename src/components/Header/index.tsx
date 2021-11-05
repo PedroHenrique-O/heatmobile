b9657e470da4eb8d94fce5c8c1f6c5e14c1fd70e
Pedro <pedrokaz.henrique@gmail.com>
2021-11-05 19:57:29 -0300

    commit3

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,12 +11,6 @@ export function Header() {
     <View style={styles.container}>
       <LogoSvg style={styles.logo} />
       <View style={styles.logout}>
-<<<<<<< Updated upstream
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>Sair</Text>
-        </TouchableOpacity>
-        <UserPhoto imageUri="https://github.com/PedroHenrique-O.png" />
-=======
         {user && (
           <TouchableOpacity>
             <Text style={styles.logoutText} onPress={signOut}>
@@ -25,7 +19,6 @@ export function Header() {
           </TouchableOpacity>
         )}
         <UserPhoto imageUri={user?.avatar_url} />
->>>>>>> Stashed changes
       </View>
     </View>
   );
